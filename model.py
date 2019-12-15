fcaf24048c30452165ebe2f48ae3078f19aea8bb
DhineshKs <dhineshksd1997@gmail.com>
2019-12-15 15:14:52 +0530

    image modulation for face and eyes

--- model.py
+++ model.py
@@ -25,3 +25,58 @@ eye = ['Closed','Open']
 
 #load CNN model
 model = load_model('/cnnmodel/cnn.h5')
+#initialize webcam
+capture = cv2.VideoCapture(0)
+
+font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+count = 0
+score = 0 
+thres = 2
+rightpred = [99]
+leftpred = [99]
+
+while(True):
+	ret, frame = capture.read()
+	height,width = frame.shape[:2]
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	faces = face.detectMultiScale(gray,minNeighbors=5,scaleFactor=1.1,minSize=(25,25))
+	left_eye = lefteye.detectMultiScale(gray)
+    right_eye = righteye.detectMultiScale(gray)
+	cv2.rectangle(frame, (0,height-50) , (200,height) , (0,0,0) , thickness=cv2.FILLED )
+	for (x,y,w,h) in faces:
+        cv2.rectangle(frame, (x,y) , (x+w,y+h) , (255,0,0) , 1 )
+	for (x,y,w,h) in right_eye:
+        r_eye=frame[y:y+h,x:x+w]
+        count=count+1
+        r_eye = cv2.cvtColor(r_eye,cv2.COLOR_BGR2GRAY)
+        r_eye = cv2.resize(r_eye,(24,24))
+        r_eye= r_eye/255
+        r_eye= r_eye.reshape(24,24,-1)
+        r_eye = np.expand_dims(r_eye,axis=0)
+        rightpred = model.predict_classes(r_eye)
+        if(rightpred[0]==1):
+            eye ='Open'
+        if(rightpred[0]==0):
+            eye ='Closed'
+        break
+	for (x,y,w,h) in left_eye:
+        l_eye=frame[y:y+h,x:x+w]
+        count=count+1
+        l_eye = cv2.cvtColor(l_eye,cv2.COLOR_BGR2GRAY)
+        l_eye = cv2.resize(l_eye,(24,24))
+        l_eye= l_eye/255
+        l_eye=l_eye.reshape(24,24,-1)
+        l_eye = np.expand_dims(l_eye,axis=0)
+        leftpred = model.predict_classes(l_eye)
+        if(leftpred[0]==1):
+            eye ='Open'
+        if(leftpred[0]==0):
+            eye ='Closed'
+        break
+	if(rightpred[0]==0 and leftpred[0]==0):
+        score=score+1
+        cv2.putText(frame,"Closed",(10,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
+	else:
+        score=score-1
+        cv2.putText(frame,"Open",(10,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
+
